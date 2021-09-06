f75614b0f1548f560eb580a26c8897bb68ce1854
Denis Kirgizov <arslan14.01.09@gmail.com>
2021-09-06 11:39:16 +0500

    feat: добавить таблицу для вывода моделей

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,62 @@
 @extends('adminlte::page')
 
+@section('plugins.Datatables', true)
+{{-- Setup data for datatables --}}
 @section('content_header')
     <h1>Dashboard</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <!-- /.card -->
+                    <div class="card">
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>Заголовок</th>
+                                    <th>Ссылка на ресурс</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($notices as $notice)
+                                <tr>
+                                    <td>{{$notice->id}}</td>
+                                    <td>{{$notice->title}} </td>
+                                    <td><a target="_blank" href="{{$notice->link}}">{{$notice->link}}</a></td>
+                                </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+
+
 @stop
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
 
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+
+
+
+
+
+
+
+
+
+
+
