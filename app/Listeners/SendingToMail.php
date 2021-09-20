5db3378a70777bee96554ce8d95a6ce28b7c3679
Denis Kirgizov <arslan14.01.09@gmail.com>
2021-09-20 10:30:54 +0500

    feat: добавить тест для обработки событий в очереди

--- app/Listeners/SendingToMail.php
+++ app/Listeners/SendingToMail.php
@@ -3,12 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\NoticesParsed;
-use App\Jobs\Logging;
 use App\Jobs\MailSend;
-use App\Mail\NotifyAboutNotices;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\Mail;
 
 class SendingToMail
 {
