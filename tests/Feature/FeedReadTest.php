0ae681de182af4468db09c69fad7b481a21af0b7
Denis Kirgizov <arslan14.01.09@gmail.com>
2021-09-23 14:50:36 +0500

    style: добавить комментарии для определения действий

--- tests/Feature/FeedReadTest.php
+++ tests/Feature/FeedReadTest.php
@@ -14,18 +14,18 @@ class FeedReadTest extends TestCase
 {
     public function test_checksSendingRequests()
     {
+        //arrange
         $xml = "<channel>
                 <item><title>В учреждениях культуры Башкирии </title>
                 <link>https://www.rietdbk.plde0fo</link></item>
                 </channel>";
-
         Http::fake(function () use ($xml){
             return Http::response($xml);
         });
-
+        //act
         $instance = new FeedReadComponent();
         $response = $instance->read();
-
+        //assert
         $this->assertObjectHasAttribute('title', $response[0]);
         $this->assertSame($response[0]->title,'В учреждениях культуры Башкирии');
         Http::assertSent(function (Request $request){
