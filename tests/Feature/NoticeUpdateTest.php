0e52b9bf13ab1a76dd088b03b1fd7aa99aa8162a
Denis Kirgizov <arslan14.01.09@gmail.com>
2021-09-26 20:01:02 +0500

    fix: изменить тест контроллера обновления новостей

--- tests/Feature/NoticeUpdateTest.php
+++ tests/Feature/NoticeUpdateTest.php
@@ -2,6 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Components\FeedReadComponent;
+use App\Components\NoticeRefreshComponent;
+use App\Components\ParseNoticeDTO;
+use App\Http\Controllers\NoticeUpdateController;
+use App\Http\Controllers\RefreshNoticesCommand;
+use App\Models\Notice;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -9,13 +15,53 @@ use Tests\TestCase;
 
 class NoticeUpdateTest extends TestCase
 {
-    public function test_if_user_is_not_admin_no_button()
+    public function test_if_admin_button_is_visible()
     {
-        //arrange
-        $user = User::factory()->create(['roles'=>'user']);
-        //act
-        $response = $this->actingAs($user)->get('/');
-        //assert
-        $response->assertDontSeeText('Обновить');
+        // Arrange
+        $admin = User::factory()->admin()->make();
+
+        // Act
+        $response = $this->actingAs($admin);
+
+        //Assert
+        $response->get('/')->assertSeeText('Обновить');
+    }
+
+    public function test_if_admin_access_by_route_is_allowed()
+    {
+        // Arrange
+        $admin = User::factory()->admin()->make();
+        Notice::factory()->create(['title'=>'old notice']);
+        $this->actingAs($admin)
+            ->get('/')
+            ->assertSee('old notice');
+        $mock = $this->createMock(FeedReadComponent::class);
+        $mock->expects(self::exactly(1))
+            ->method('read')
+            ->willReturn([new ParseNoticeDTO('new notice','htpps//www.ddw.ww')]);
+        $instance = new NoticeUpdateController();
+        $refresh = new NoticeRefreshComponent();
+
+        // Act
+        $response = $this->post('/update')->assertRedirect('/');
+        $instance->update($mock, $refresh);
+
+        // Assert
+        $response = $this->followRedirects($response)->assertOk();
+        $response->assertSee('new notice');
+        $response->assertDontSee('old notice');
+    }
+
+    public function test_if_moderator_button_is_hidden_and_not_access_by_route()
+    {
+        // Arrange
+        $moderator = User::factory()->moderator()->make();
+
+        // Act
+        $response = $this->actingAs($moderator);
+
+        // Assert
+        $response->post('/update')->assertForbidden();
+        $response->get('/')->assertDontSee('Обновить');
     }
 }
