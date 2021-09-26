7811e461d7a5ecea32dc35d91a0e2023bd0bb052
Denis Kirgizov <arslan14.01.09@gmail.com>
2021-09-26 18:28:35 +0500

    fix: добавить в контроллере правило авторизации

--- app/Http/Controllers/NoticeUpdateController.php
+++ app/Http/Controllers/NoticeUpdateController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Components\FeedReadComponent;
 use App\Components\NoticeRefreshComponent;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class NoticeUpdateController extends Controller
 {
     public function update(FeedReadComponent $reader, NoticeRefreshComponent $refresher)
     {
+        if (!Gate::allows('can_parse_notices')){
+            abort(403);
+        }
         $notices = $reader->read();
         $refresher->refresh($notices);
 
